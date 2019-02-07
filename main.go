4fa6b2b2b4c9b5964f24e3522d679c0e9c651a73
M. Y. Katz <mykatz01@gmail.com>
2019-02-07 15:05:50 -0800

    Created file upload helper function, begun working on SIA integration

--- main.go
+++ main.go
@@ -10,7 +10,12 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("yo!")
 		fmt.Fprintf(w, "Hello world!")
+	})
+
+	r.HandleFunc("/sendFile", func(w http.ResponseWriter, r *http.Request) {
+		//upload to SIA
 	})
 
 	http.ListenAndServe(":80", r)
---
+++ pkg/uploadToSia.go
@@ -0,0 +1,30 @@
+package uploadToSia
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func upload(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		r.ParseMultipartForm(32 << 20) //Parses data, w/ filesize limit of 32MiB
+		file, handler, err := r.FormFile("fileUpload")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		pushToSia(file)
+	} else {
+		fmt.Println("Wrong method. Expected POST")
+	}
+
+}
+
+func pushToSia(f http.File) {
+	fmt.Println("pushing file...")
+	//send file to SIA blockchain
+}
