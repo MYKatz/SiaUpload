d3780b528afac0211edfb8293350a59558daa056
M. Y. Katz <mykatz01@gmail.com>
2019-02-10 15:19:28 -0800

    Routed to homepage

--- main.go
+++ main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"html/template"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
+	
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./web/assets/"))))
+
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("yo!")
-		fmt.Fprintf(w, "Hello world!")
+		//send homepage
+		tmp, err := template.ParseFiles("web/index.html")
+		data := ""
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+        	return
+		}
+
+		if err := tmp.Execute(w, data); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		fmt.Printf("Served homepage")
 	})
 
 	r.HandleFunc("/sendFile", func(w http.ResponseWriter, r *http.Request) {
