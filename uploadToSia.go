05e3db965143844b509a45a9a1792c97d6906c57
M. Y. Katz <mykatz01@gmail.com>
2019-02-11 15:34:17 -0800

    Finished file upload temporarily saving to disk

--- uploadToSia.go
+++ uploadToSia.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"net/http"
 	"mime/multipart"
+	"github.com/google/uuid"
 )
 
 func upload(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == "POST" {
 		r.ParseMultipartForm(32 << 20) //Parses data, w/ filesize limit of 32MiB
-		file, _, err := r.FormFile("fileUpload")
+		file, header, err := r.FormFile("fileUpload")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer file.Close()
-		fmt.Fprintf(w, "Got file.")
-		pushToSia(file)
+		//writes file to UUID + ___ + filename in tmp folder
+		out, err := os.Create("./tmp/" + uuid.New().String() + "___" + header.Filename)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "File uploaded successfully")
+		//pushToSia(file)
 	} else {
 		fmt.Println("Wrong method. Expected POST")
 	}
